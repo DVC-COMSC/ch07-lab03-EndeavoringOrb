55a6a09ce4e56b7680c0942d8a658bc085b25cc5
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 13:08:28 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+numbers = []
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(5):
+    numbers.append(int(input))
+
+avg = sum(numbers)/len(numbers)
+count = 0
+
+for i in (numbers):
+    if numbers[i] > avg:
+        count += 1
